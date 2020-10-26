2991fbeea006cf5642adc436b3740a8554f28437
bgbrenlla <72096031+bgbrenlla@users.noreply.github.com>
2020-10-26 17:23:17 +0100

    Update ipm-p1.py

--- ipm-p1.py
+++ ipm-p1.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from controller import Controller
 import model
 import view
+import locale
+import gettext
+from pathlib import Path
+
+locale.setlocale(locale.LC_ALL,'')
+
+locale.bindtextdomain("ipm-p1",Path(__file__).parent / "locale")
+gettext.bindtextdomain("ipm-p1",Path(__file__).parent / "locale")
+gettext.textdomain("ipm-p1")
 
 if __name__ == '__main__':
 
@@ -11,3 +21,4 @@ if __name__ == '__main__':
     controller.set_view(view.View())
     controller.set_error(view)
     controller.main()
+
